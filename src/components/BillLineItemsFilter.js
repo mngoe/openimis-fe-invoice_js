627e1d4ff8fcccae3f62892b0c420d05926f1b8d
Maxime NGOE <maxime.ngoe@gmail.com>
2026-09-09 15:06:23 +0100

    Update BillLineItemsFilter.js

--- src/components/BillLineItemsFilter.js
+++ src/components/BillLineItemsFilter.js
@@ -13,7 +13,6 @@ const styles = (theme) => ({
     padding: theme.spacing(1),
   },
 });
-
 const BillLineItemsFilter = ({ classes, filters, onChangeFilters }) => {
   const debouncedOnChangeFilters = _debounce(onChangeFilters, DEFUALT_DEBOUNCE_TIME);
 
